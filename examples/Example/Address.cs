18d1fafbedb45e7a0d4441069c65ac42e2116687
Rahadian <rahadian@frontier.cool>
2025-11-10 00:04:42 +0700

    delete examples

--- examples/Example/Address.cs
+++
@@ -1,9 +0,0 @@
-namespace Example;
-
-class Address
-{
-    public string Id { get; set; }
-    public int HouseNumber { get; set; }
-    public string AccessAddress { get; set; }
-    public string MetadataNotes { get; set; }
-}
